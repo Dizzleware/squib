12dc73bbe159ad9176dbf92c392d2f0887d58dc6
TorontoRage <braycayle@gmail.com>
2014-02-28 13:57:19 -0500

    intellij Project
    IDE specific projects

---
+++ src/entity.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Cayle J. Elsey on 2/27/14.
+ */
+public interface entity {
+    enum DIRECTION{
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST;
+    }
+
+    enum COLOR{
+        RED,
+        BLACK,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        PURPLE;
+    }
+
+    public void place(Point p);
+    public void move(DIRECTION dir);
+    public void convert(COLOR color);
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Cayle J. Elsey on 2/27/14.
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.print("SWAGGER");
+
+    }
+}
+}
